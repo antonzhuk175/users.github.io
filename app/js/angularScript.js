1ac8f7f6b6ae29a667b44f902a309d0ed4ee2bcb
antonzhuk175 <juk01anton96@narod.ru>
2016-12-03 20:05:03 +0300

    Added info on modal window, fixed some details

--- app/js/angularScript.js
+++ app/js/angularScript.js
@@ -2,5 +2,22 @@ var usersApp = angular.module('usersApp', []);
 usersApp.controller('usersController', function($scope, $http){
     $http.get('http://jsonplaceholder.typicode.com/users').success(function(data){
         $scope.users = data;
+        $scope.showModalWin = function(index) {
+
+            var darkLayer = document.createElement('div');
+            darkLayer.id = 'shadow';
+            document.body.appendChild(darkLayer);
+
+            var modalWin = document.getElementById('mWin');
+            modalWin.style.display = 'block';
+
+            darkLayer.onclick = function() {
+                darkLayer.parentNode.removeChild(darkLayer);
+                modalWin.style.display = 'none';
+                return false;
+            };
+
+            $scope.user = data[index];
+        }
     })
 });
